581b2871bfca29b454f64eaf5d08a9f155a68932
milkyarmhkhk <jmandre@hkhk.edu.ee>
2025-12-15 12:27:25 +0200

    Update Harjutus2.2.py

--- Harjutus2.2.py
+++ Harjutus2.2.py
@@ -1,27 +1,49 @@
-#Jarek Mandre (15.10.2025)
-#Harjutus 2.1
+#Jarek Mandre
+#10.11.25
 
 import turtle
-
+t = turtle.Turtle()
 aken = turtle.Screen()
 aken.setup(width=500, height=500)
-aken.title("Sinilill, Jarek")
-t = turtle.Turtle()
-t.speed("fastest")
+aken.title("Sinilill - Jarek")
+
+
+#vars
+t.penup()
+t.goto(0,-200)
+t.pendown()
 t.pensize(10)
+t.pencolor("green")
+t.lt(90)
+t.fd(200)
+#õis
+t.begin_fill()
+t.pensize(10)
+t.color("blue")
+t.rt(90)
+t.circle(60)
+t.end_fill()
+
+##emakas
+t.penup()
+t.goto(0,50)
+t.pendown()
+t.pencolor("yellow")
+t.pensize(20)
+t.circle(10)
 
 t.penup()
-t.goto(0,100)
+t.goto(-20,-100)
 t.pendown()
-t.pencolor("green")
-t.right(90)
-t.fd(200)
-t.penup()
-t.right(90)
-t.goto(0,50)
-t.fd(10)
-for i in range(3):
-    turtle.fd(200)
-    turtle.lt(120)
+t.pensize(10)
+t.begin_fill()
+t.color("green", "green")
+t.lt(90)
+t.fd(40)
+t.lt(135)
+t.fd(35)
+t.lt(100)
+t.fd(30)
+t.end_fill()
 
 turtle.done()
