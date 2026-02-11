392bb842026aec2aa578e382c3a6b6acfea3cacd
milkyarmhkhk <jmandre@hkhk.edu.ee>
2026-02-11 09:55:29 +0200

    11.02

--- Harjutus13.py
+++ Harjutus13.py
@@ -9,20 +9,27 @@ pikk = 10
 t.penup()
 t.goto(-450, 0)
 t.pendown()
-number = 1
+number = 0
 for i in range(1, int(pikkus)):
     t.lt(90)
     t.fd(20+pikk)
-    t.write(number, font=("Arial", 12, "normal"))
     t.bk(20+pikk)
     t.rt(90)
     t.fd(20)
     if i%5==0:
         pikk = 10
-        number = i
     else:
         pikk = 0
-        #number = " "
-    number += i
+
+t.penup()
+t.goto(-450, 0)
+t.lt(90)
+t.fd(20+pikk+10)
+t.rt(90)
+for i in range(0,int(pikkus)):
+    if i%5==0:
+        t.write(number, font=("Arial", 8, "normal"))
+    t.fd(20)
+    number += 1
 
 t.done()
