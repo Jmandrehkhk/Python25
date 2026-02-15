3d558bc347a0ed579de6a1afbae90e7df647730d
Jmandre <jmandre@hkhk.edu.ee>
2026-02-15 17:21:53 +0200

    15.02(v2)

--- Pythonistoo/4.Iseseisevtöö.py
+++ Pythonistoo/4.Iseseisevtöö.py
@@ -68,17 +68,17 @@ import math
 # print(pronksikarva_summa())
 
 #4.6
-print(list)
-months = ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"]
-def get_month_name(number):
-    month_name = months[number-1]
-    return month_name
-def time_convert_to_formatted(time):
-    list = time.split(".")
-    month = get_month_name(int(list[1]))
-    finished_format = f"{list[0]}. {month} {list[2]}. a"
-    return finished_format
-
-
-time = input("Sisesta kuupäev kujul DD.MM.YYYY: ")
-print(time_convert_to_formatted(time))
+# print(list)
+# months = ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"]
+# def get_month_name(number):
+#     month_name = months[number-1]
+#     return month_name
+# def time_convert_to_formatted(time):
+#     list = time.split(".")
+#     month = get_month_name(int(list[1]))
+#     finished_format = f"{list[0]}. {month} {list[2]}. a"
+#     return finished_format
+# 
+# 
+# time = input("Sisesta kuupäev kujul DD.MM.YYYY: ")
+# print(time_convert_to_formatted(time))
